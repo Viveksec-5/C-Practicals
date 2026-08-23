7f132e9367186c5f46f7bff81a66a2a7d655b6cb
Viveksec-5 <mauryapratima470@gmail.com>
2026-08-23 22:01:17 +0530

    factorial problem

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int product = 1, n;
+    printf("enter a number: \n");
+    scanf("%d", &n);
+    if (n < 0)
+    {
+        printf("this is a negative number \n");
+    }
+
+    else
+    {
+
+        for (int i = 1; i <= n; i++)
+        {
+            product = product * i;
+        }
+        printf("the factorial of this number is %d \n", product);
+    }
+
+    return 0;
+}
