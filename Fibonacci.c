e6a78485513c8c035d5f9183124642a30bf6f9dc
Viveksec-5 <mauryapratima470@gmail.com>
2026-09-10 20:16:06 +0530

    second init

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+//write a function to print nth term of Fibonacci sequence.
 int fib(int n);
 int main()
 {
     int n;
-     printf("%d\n",fib(6));
-     
+    printf("%d\n", fib(6));
 
     return 0;
 }
@@ -26,7 +26,6 @@ int fib(int n)
     int fibNm1 = fib(n - 1);
     int fibNm2 = fib(n - 2);
     int fibn = fibNm1 + fibNm2;
-    
-    
+
     return fibn;
 }
