8f6dba29f50a5e242698729b0c8f15784469f8eb
Viveksec-5 <mauryapratima470@gmail.com>
2026-09-14 16:37:27 +0530

    To merge the strings

---
+++ strcat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+// NOTE: apne aap se gap nhi ayega khud se gap daalna hoga.
+
+int main()
+{
+    char firstr[] = "Vivek ";
+    char secstr[] = "Maurya";
+    strcat(firstr, secstr);
+    puts(firstr);
+    return 0;
+}
